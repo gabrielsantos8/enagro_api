7cf22bbe199ed894ccbd9cc1d2f310b42870bbb2
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-09-30 18:57:07 -0300

    FEAT: colunas situação

---
+++ database/migrations/2023_09_30_215015_add_collumn_status_in_veterinarians_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('veterinarians', function (Blueprint $table) {
+            $table->unsignedBigInteger('situation_id')->default(1);
+            $table->foreign('situation_id')->references('id')->on('situations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('veterinarians', function (Blueprint $table) {
+            //
+        });
+    }
+};
