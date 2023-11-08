21fb0dbb321a672df7773ac16981b248aee071bd
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-11-07 22:24:17 -0300

    FEAT: inicio parcelas por usuario

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'relatory/plansByRegionData'
+        'relatory/plansByRegionData',
+        'relatory/installmentByUserIndexData'
     ];
 }
